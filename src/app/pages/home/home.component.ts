fe1c6d1aef36b94d4c9359542b24cbef1976e56c
felipefalkiner <felipe.magosso@gmail.com>
2023-10-01 17:24:17 -0300

    home component now pulls data from dataFake.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { dataFake } from '../../data/dataFake'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  data = dataFake;
 
   constructor() { }
 
